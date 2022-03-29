35a58600e410a201327725209c1f6de494d92dbe
Stanislava Zarkova <47562159+szarkoo@users.noreply.github.com>
2022-03-29 16:55:46 +0100

    Update and rename cw.py to upload_to_s3.py

--- CW/cw.py
+++
@@ -1,8 +0,0 @@
-from time import time, sleep
-import os
-i = 0
-a_directory = "images/images/"
-for filename in os.listdir(a_directory):
-    filepath = os.path.join(a_directory, filename)
-    print(filepath)
-    sleep(10)
---
+++ CW/upload_to_s3.py
@@ -0,0 +1,46 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+from time import time, sleep
+
+#logger config
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s: %(levelname)s: %(message)s')
+
+#connecting to boto3 client
+s3_client = boto3.client('s3')
+
+def upload_file(file_name, bucket, object_name=None):
+    """Uploads a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: Event response if file was uploaded, else raise Exception
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        logger.info(f'Successfully uploaded {file_name} to S3 bucket {bucket}.')
+        return response
+    
+    except ClientError:
+        logger.exception(f'Could not upload {file_name} to S3 bucket {bucket}.')
+        raise
+        
+if __name__ == '__main__':
+    a_directory = "images/images/"
+    bucket_name="buckets1827468"
+    for filename in os.listdir(a_directory):
+        filepath = os.path.join(a_directory, filename)
+        logger.info(f'Uploading {filepath} to S3 bucket {bucket_name}...')
+        upload_file(filepath,bucket_name)
+        sleep(30)
+    

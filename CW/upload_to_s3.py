e0bb4d59ed3c64bf795721910606a5b355729bd3
Stanislava Zarkova <47562159+szarkoo@users.noreply.github.com>
2022-03-30 11:28:02 +0100

    Update upload_to_s3.py

--- CW/upload_to_s3.py
+++ CW/upload_to_s3.py
@@ -18,29 +18,37 @@ def upload_file(file_name, bucket, object_name=None):
     :param file_name: File to upload
     :param bucket: Bucket to upload to
     :param object_name: S3 object name. If not specified then file_name is used
-    :return: Event response if file was uploaded, else raise Exception
+    :return: Event response if file was uploaded, None if file is not an image, else raise Exception
     """
 
     # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = os.path.basename(file_name)
+        
+    if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+      
+      # Upload the file
+      try:
+          response = s3_client.upload_file(file_name, bucket, object_name)
+          logger.info(f'Successfully uploaded {file_name} to S3 bucket {bucket}.')
+          sleep(30)
+          return response
 
-    # Upload the file
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-        logger.info(f'Successfully uploaded {file_name} to S3 bucket {bucket}.')
-        return response
-    
-    except ClientError:
-        logger.exception(f'Could not upload {file_name} to S3 bucket {bucket}.')
-        raise
+      except ClientError:
+          logger.exception(f'Could not upload {file_name} to S3 bucket {bucket}.')
+          raise
+    else:
+        logger.info(f'File is not an image')
+        return None
         
 if __name__ == '__main__':
     a_directory = "images/images/"
     bucket_name="buckets1827468"
+    if len(os.listdir(a_directory) ) == 0:
+      logger.info(f'Directory is empty. No files to upload.')
+    else:
     for filename in os.listdir(a_directory):
         filepath = os.path.join(a_directory, filename)
         logger.info(f'Uploading {filepath} to S3 bucket {bucket_name}...')
         upload_file(filepath,bucket_name)
-        sleep(30)
     
